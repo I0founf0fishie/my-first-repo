f6584ae49bd8065714c251de487e4113acb8542c
Foshi <22.06.2012mary@gmail.com>
2026-02-12 19:08:32 +0300

    showing an image

--- hello world.py
+++ hello world.py
@@ -1,1 +1,37 @@
-print("Hello world!") 
+import pygame
+import sys
+
+
+pygame.init()  
+
+SCREEN_WIDTH = 600  
+SCREEN_HEIGHT = 600 
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Tile from Tileset")
+
+tileset = pygame.image.load("cloud_1.jpg").convert_alpha()
+
+
+clock = pygame.time.Clock()  
+
+while True:
+   
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() 
+            sys.exit()    
+
+    screen.fill((255, 0, 255))
+
+
+    screen.blit(
+        tileset,
+        (0, 0)
+    )
+
+  
+    pygame.display.flip()
+
+    clock.tick(60)
